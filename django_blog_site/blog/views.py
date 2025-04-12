8e819b641c2bf1a21ccbb1855919b6ce4c76da90
hoondori <hoondori@gmail.com>
2025-04-12 09:02:34 +0900

    pagination

--- django_blog_site/blog/views.py
+++ django_blog_site/blog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage
 from .models import Post
 
+
 def post_list(request):
-  posts = Post.published.all()
-  return render(request, 'blog/post/list.html', {'posts': posts})
+  all_posts = Post.published.all()
+
+  # restrict number of posts displayed in one page to enhance readability
+  paginator = Paginator(all_posts, 3)
+  page_number = request.GET.get('page', 1)
+  try:
+    posts = paginator.page(page_number)
+  except EmptyPage:
+    # if the requested page number is not valid, use last available page instead.
+    posts = paginator.page(paginator.num_pages)
+
+  return render(request, 
+                'blog/post/list.html', 
+                {'posts': posts})
 
 def post_detail(request, year, month, day, slug):
   # try:
