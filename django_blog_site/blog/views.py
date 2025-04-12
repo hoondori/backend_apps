0ec901742fa83bae0f4882ff84f9abd2294a9696
hoondori <hoondori@gmail.com>
2025-04-12 20:19:44 +0900

    find similar posts based on tags

--- django_blog_site/blog/views.py
+++ django_blog_site/blog/views.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
   all_posts = Post.published.all()
@@ -28,11 +29,6 @@ def post_list(request, tag_slug=None):
                 {'posts': posts, 'tag': tag})
 
 def post_detail(request, year, month, day, slug):
-  # try:
-  #   post = Post.published.get(id=id)
-  # except Post.DoesNotExist:
-  #   raise Http404("No Post found.")
-
   # for better SEO
   post = get_object_or_404(
     Post, 
@@ -48,9 +44,17 @@ def post_detail(request, year, month, day, slug):
   # comment form 
   form = CommentForm()
 
+  # fine similiar posts based on tags
+  post_tags_ids = post.tags.values_list('id', flat=True)
+
+  similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                .exclude(id=post.id)
+
+  similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                               .order_by('-same_tags', '-publish')[:4]
   return render(request, 
                 'blog/post/detail.html', 
-                {'post': post, 'comments': comments, 'form': form})
+                {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 def post_share(request, post_id):
 
